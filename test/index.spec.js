9055037d4d39955dea3b3413d1577f8c991507e6
Laboratória <ma-ah02@live.com>
2019-05-03 14:28:54 -0300

    implementando testes

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,1 +1,15 @@
+let chai = require("chai");
+let expect = chai.expect;
+let creditCard = require("../lib/index.js");
 
+describe("Retornar true para cartão de crédito válido", () => {
+    it("Deve retornar true para um cartão de crédito válido", () => {
+        expect(creditCard.cardValidator(4916460392805686)).to.equal(true);
+    })
+}) 
+
+describe("Retornar false para cartão de crédito inválido", () => {
+    it("Deve retornar false para um cartão de crédito inválido", () => {
+        expect(creditCard.cardValidator(12345678910)).to.equal(false);
+    })
+})
